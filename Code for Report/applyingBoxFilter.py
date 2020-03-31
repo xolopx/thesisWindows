988b236a88031b1deb9b2a7daa2ad137b2d4dd98
Unknown <xolopx@gmail.com>
2020-04-01 01:06:36 +1100

    Improved Report

--- Code for Report/applyingBoxFilter.py
+++ Code for Report/applyingBoxFilter.py
@@ -4,8 +4,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 import cv2 as cv
 
-# Get image
-im = Image.open("/home/tom/Desktop/thesis/Report/images/dogPug.jpg")
+# Open image and store in an array
+im = Image.open(r"C:\Users\Tom\Desktop\thesisWindows\Report\images\dogPug.jpg")
 im = np.array(im)
 
 # Apply filter
