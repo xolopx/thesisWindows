5e716822afca09f74e98b2a5ecc9e2679dc9a6ba
Unknown <xolopx@gmail.com>
2020-04-02 16:18:41 +1100

    More LitRev

---
+++ Code for Report/correlation.m
@@ -0,0 +1,31 @@
+im = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\wall.png");
+%im1 = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\horizontal.png");
+%im2 = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\vertical.png");
+im = rgb2gray(im);
+
+[gv, t] = edge(im, 'sobel', 0.15,'vertical');
+[gh, z] = edge(im, 'sobel', 0.15,'horizontal');
+
+gv = uint8(255 * gv);
+gh = uint8(255 * gh);
+multi = cat(2,im, gv, gh);
+
+imshow(im)
+
+%%
+
+
+
+
+%% 
+%kernel = fspecial('sobel');
+kernelV = [-1 2 -1; -1 2 -1; -1 2 -1];
+kernelH = [-1 -1 -1; 2 2 2; -1 -1 -1];
+
+%% Next section
+resultV = imfilter(im, kernelV);
+resultH = imfilter(im, kernelH);
+multi = cat(2, im, resultV, resultH);
+%imshow(result,[])
+figure();
+montage(multi);
