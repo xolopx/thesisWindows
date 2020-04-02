e2612f646e26e618ecbf09cc89f92a2b620f6f83
Unknown <xolopx@gmail.com>
2020-04-02 18:06:28 +1100

    Added to Correlation of Lit Rev

--- Code for Report/asymmetrical_kernel.m
+++ Code for Report/asymmetrical_kernel.m
@@ -2,6 +2,7 @@ A = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\van.jpg");
 A = rgb2gray(A);
 %imshow(im)
 kernel = ones(16,16)/(16^2);
-
-localMean = convn(A,15);
-imshowpair(A,localMean,'montage')
+kern = fspecial('sobel');
+kernel = cast(kernel, 'like',kern);
+result = imfilter(A, kernel);
+imshow(result)
--- Code for Report/correlation.m
+++ Code for Report/correlation.m
@@ -1,7 +1,7 @@
-im = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\wall.png");
+im_color = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\wall.png");
 %im1 = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\horizontal.png");
 %im2 = imread("C:\Users\Tom\Desktop\thesisWindows\Report\images\vertical.png");
-im = rgb2gray(im);
+im = rgb2gray(im_color);
 
 [gv, t] = edge(im, 'sobel', 0.15,'vertical');
 [gh, z] = edge(im, 'sobel', 0.15,'horizontal');
@@ -10,7 +10,7 @@ gv = uint8(255 * gv);
 gh = uint8(255 * gh);
 multi = cat(2,im, gv, gh);
 
-imshow(im)
+imshow(im_color)
 
 %%
 
