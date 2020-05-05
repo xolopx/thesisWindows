bce0bbb4b5e181e65d49ad87e04864056d0c297f
Unknown <xolopx@gmail.com>
2020-05-05 18:09:46 +1000

    Removed redundant files etc. Got streaming from CV algorithm to work.

--- System/testDetection.py
+++
@@ -1,9 +0,0 @@
-""" Written for the purpose of testing this system submodule."""
-import cv2 as cv
-from detection import *
-
-
-# Detection
-inputVideo = cv.VideoCapture(r"C:\Users\Tom\Desktop\thesisWindows\System\detection\traffic_short.mp4")
-process = CVModule.CVModule(inputVideo)
-process.process()
