007941e470f009f74c918284d3eebaede5b492f7
tom <xolopx@gmail.com>
2020-06-10 14:23:55 +1000

    Made paths relative.

--- System/database_interface/python_mysql_dbconfig.py
+++ System/database_interface/python_mysql_dbconfig.py
@@ -1,15 +1,25 @@
 from configparser import ConfigParser
+import os
 
 
-def read_db_config(filename=r'C:\Users\Tom\Desktop\thesisWindows\System\database_interface\config.ini', section='mysql'):
+def read_db_config(filename = 'config.ini', section='mysql'):
     """ Read database configuration file and return a dictionary object
-    :param filename: name of the configuration file
+    :param filename: Config file name
     :param section: section of database configuration
     :return: a dictionary of database parameters
     """
+    dirname = os.path.dirname(__file__)
+    path = os.path.join(dirname, filename)
     # create parser and read ini configuration file
     parser = ConfigParser()
-    parser.read(filename)
+    try:
+       f = open(path)
+    except FileNotFoundError as e:
+        print(e)
+    finally:
+       f.close()
+
+    parser.read(path)
 
     # get section, default to mysql
     db = {}
@@ -18,7 +28,7 @@ def read_db_config(filename=r'C:\Users\Tom\Desktop\thesisWindows\System\database
         for item in items:
             db[item[0]] = item[1]
     else:
-        msg = "{0} not found in the {1} file".format(section, filename)
+        msg = "{0} not found in the {1} file".format(section, path)
         raise Exception(msg)
 
     return db
